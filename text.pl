ba332e3e1e512e1134c19a4f660b572432237d7d
Amanda Folson <amanda@incredibl.org>
2013-12-25 14:34:58 -0500

    Add license header and minor comments.

--- text.pl
+++ text.pl
@@ -1,4 +1,21 @@
 #!/usr/bin/perl
+# irssi-text An irssi script that will text your phone when you are /away and someone PMs you.
+# Copyright (C) 2013  Amanda Folson <amanda@incredibl.org>
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 use warnings;
 use strict;
 use Irssi;
@@ -11,14 +28,14 @@ my %IRSSI = (
 	contact		=> 'amanda@incredibl.org',
 	name		=> 'irssi-text',
 	uri		=> 'https://github.com/afolson/irssi-text',
-	description	=> 'A script that will text your phone when you are /away and someone PMs you.',
+	description	=> 'An irssi script that will text your phone when you are /away and someone PMs you.',
 	license		=> 'GPL',
 );
 
 # Ha, load.
 my $load = "$IRSSI{name} $IRSSI{version} by $IRSSI{author} <$IRSSI{contact}>";
 
-# Add a switch for this so I don't have to unload it anymore :D?
+# Toggle paging/texting on or off so the script doesn't need to be unloaded. Defaults to ON.
 Irssi::settings_add_bool('text', 'paging_enabled', 1);
 Irssi::settings_add_str('text', 'your_name', 'irssi');
 Irssi::settings_add_str('text', 'sms_address', '');
@@ -51,4 +68,5 @@ sub send_email {
 }
 
 Irssi::signal_add("message private", \&text);
+# Print out some information about the script when it's loaded.
 Irssi::print "$load";
